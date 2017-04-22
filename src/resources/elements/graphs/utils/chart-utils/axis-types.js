201a0b4da02da8881d454fd2c096f287726e667c
Tanel <tanel.prikk@hotmail.com>
2017-04-22 14:17:39 +0300

    done

---
+++ src/resources/elements/graphs/utils/chart-utils/axis-types.js
@@ -0,0 +1,17 @@
+export const typeMap = {
+  'time': {
+    time: {
+      displayFormats: {
+        'millisecond': 'MMM DD',
+        'second': 'MMM DD',
+        'minute': 'MMM DD',
+        'hour': 'MMM DD',
+        'day': 'MMM DD',
+        'week': 'MMM DD',
+        'month': 'MMM DD',
+        'quarter': 'MMM DD',
+        'year': 'MMM DD'
+      }
+    }
+  }
+};
